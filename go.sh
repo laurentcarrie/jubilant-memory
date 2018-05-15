dc79a1a6f77aee02bb3ea5da348b53920d5f3494
laurent.carrie <laurent.carrie@gmail.com>
2018-05-15 15:04:44 +0200

    script ameliore

--- go.sh
+++ go.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
-set -e
-set -x
+#set -e
+#set -x
 
-git log --format="%H" -n 1 > gitlog.tex
 
 #set PATH=F:\\miktex\\texmfs\\install\\miktex\\bin:$PATH
 
+#set count=`git log --format="%H"`
+count=`git status --porcelain | wc --lines | tr --delete '\r' | tr --delete '\n' | tr --delete ' '` 
 
+case $count in
+0)
+git log --format="%H" -n 1 > gitlog.tex
 pdflatex main.tex
 pdflatex main.tex
 pdflatex main.tex
+;;
+*)
+echo $count files not checked in
+git status --porcelain 
+exit 1
+;;
+esac
+
 
