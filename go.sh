5994355e4c266453f7d5bc72e9c0931e7f54c40a
laurentcarrie <laurent.carrie@gmail.com>
2018-06-04 14:39:09 +0200

    ajout copie sur dropbox

--- go.sh
+++ go.sh
@@ -4,6 +4,7 @@ set -e
 #set -x
 
 
+dropboxdir=c:/users/laurent/dropbox/cv
 
 #set PATH=F:\\miktex\\texmfs\\install\\miktex\\bin:$PATH
 
@@ -14,12 +15,15 @@ case $count in
 0)
 version=`git log --format="%H" -n 1 | tr --delete '\r' | tr --delete '\n' | tr --delete ' '` 
 git log --format="%H" -n 1 > gitlog.tex
+cvname=cv-laurent-carrie-$version.pdf
 
 rm -rf *.pdf
 pdflatex main.tex
 pdflatex main.tex
 pdflatex main.tex
-cp main.pdf cv-laurent-carrie-$version.pdf
+cp main.pdf $cvname
+rm -f $dropboxdir/cv*.pdf
+cp $cvname $dropboxdir/$cvname
 ;;
 *)
 echo $count files not checked in
